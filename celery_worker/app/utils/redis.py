ce180ad6c3e8adbdf15d4e1aabde84a4b72b6b51
Marv192 <marvel.92@inbox.ru>
2026-05-09 14:11:28 +0300

    Добавил prometheus, grafana. Сделал дашборд с отслеживанием нагрузки CPU, RAM, disk, network.  Kafka lag.  Redis errors %, redis latency

--- celery_worker/app/utils/redis.py
+++ celery_worker/app/utils/redis.py
@@ -1,10 +1,12 @@
 import logging
+import time
 from decimal import Decimal
 from typing import Optional
 
 import redis
 
 from app.config import settings
+from app.observability.metrics import redis_latency, redis_ops
 
 logger = logging.getLogger(__name__)
 
@@ -12,23 +14,36 @@ redis_client = redis.Redis(host=settings.redis_host, port=settings.redis_port, d
 
 
 def set_cache(key: str, value: Decimal, ttl: int = settings.cache_ttl):
+    latency_start = time.perf_counter()
     try:
         redis_client.setex(f"rates:{key}", ttl, str(value))
+        redis_ops.labels(operation=f"SET", status="ok").inc()
         return True
 
     except Exception as e:
+        redis_ops.labels(operation=f"SET", status="error").inc()
         logger.error(f"{key} cache write error: {e}")
         return False
 
+    finally:
+        redis_latency.labels(operation="SET").observe(time.perf_counter() - latency_start)
+
 
 def get_cached(key: str) -> Optional[Decimal]:
+    latency_start = time.perf_counter()
     try:
         result = redis_client.get(f"rates:{key}")
         if result:
+            redis_ops.labels(operation=f"GET", status="hit").inc()
             return Decimal(result)
         else:
+            redis_ops.labels(operation=f"GET", status="miss").inc()
             return None
 
     except Exception as e:
+        redis_ops.labels(operation=f"GET", status="error").inc()
         logger.error(f"{key} cache read error: {e}")
         return None
+
+    finally:
+        redis_latency.labels(operation="GET").observe(time.perf_counter() - latency_start)
